2480c0b7fcdc7dd548fc13dcf172c55475128dca
SteveStevenpoor <eeeevaeee@gmail.com>
2024-12-25 03:20:46 +0700

    Add tasks impl

--- src/main/java/tasks/Task7.java
+++ src/main/java/tasks/Task7.java
@@ -1,17 +1,22 @@
 package tasks;
 
 import common.Company;
+import common.Vacancy;
+
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Из коллекции компаний необходимо получить всевозможные различные названия вакансий
  */
 public class Task7 {
 
-  public static Set<String> vacancyNames(Collection<Company> companies) {
-    return new HashSet<>();
-  }
-
+    public static Set<String> vacancyNames(Collection<Company> companies) {
+        return companies.stream()
+                .map(Company::getVacancies)
+                .flatMap(Collection::stream)
+                .map(Vacancy::getTitle)
+                .collect(Collectors.toSet());
+    }
 }
--- src/main/java/tasks/Task9.java
+++ src/main/java/tasks/Task9.java
@@ -4,7 +4,6 @@ import common.Person;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -20,79 +19,56 @@ P.S. Код в целом рабочий (не везде), комментари
 P.P.S Здесь ваши правки необходимо прокомментировать (можно в коде, можно в PR на Github)
  */
 public class Task9 {
-
-  private long count;
-
-  // Костыль, эластик всегда выдает в топе "фальшивую персону".
-  // Конвертируем начиная со второй
-  public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::firstName).collect(Collectors.toList());
-  }
-
-  // Зачем-то нужны различные имена этих же персон (без учета фальшивой разумеется)
-  public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
-  }
-
-  // Тут фронтовая логика, делаем за них работу - склеиваем ФИО
-  public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.secondName() != null) {
-      result += person.secondName();
-    }
-
-    if (person.firstName() != null) {
-      result += " " + person.firstName();
-    }
-
-    if (person.secondName() != null) {
-      result += " " + person.secondName();
-    }
-    return result;
-  }
-
-  // словарь id персоны -> ее имя
-  public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.id())) {
-        map.put(person.id(), convertPersonToString(person));
-      }
-    }
-    return map;
-  }
-
-  // есть ли совпадающие в двух коллекциях персоны?
-  public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
+    // Removing head of a list is O(n). Better filter the stream
+    public List<String> getNames(List<Person> persons) {
+        if (persons.isEmpty()) {
+            return Collections.emptyList();
         }
-      }
+
+        return persons.subList(1, persons.size()).stream()
+                .map(Person::firstName)
+                .collect(Collectors.toList());
     }
-    return has;
-  }
 
-  // Посчитать число четных чисел
-  public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
-  }
+    // HashSet has unique elements already
+    public Set<String> getDifferentNames(List<Person> persons) {
+        return new HashSet<>(getNames(persons));
+    }
 
-  // Загадка - объясните почему assert тут всегда верен
-  // Пояснение в чем соль - мы перетасовали числа, обернули в HashSet, а toString() у него вернул их в сортированном порядке
-  void listVsSet() {
-    List<Integer> integers = IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
-    List<Integer> snapshot = new ArrayList<>(integers);
-    Collections.shuffle(integers);
-    Set<Integer> set = new HashSet<>(integers);
-    assert snapshot.toString().equals(set.toString());
-  }
+    // Join is more readable
+    public static String convertPersonToString(Person person) {
+        return String.join(" ", person.firstName(), person.secondName(), person.middleName());
+    }
+
+    // I think streams are better overall but idk about making convertPersonToString. AFAIK it's ok
+    // for such example
+    public Map<Integer, String> getPersonNames(Collection<Person> persons) {
+        return persons.stream().collect(Collectors.toMap(Person::id, Task9::convertPersonToString));
+    }
+
+    // I am not sure about consistency of persons1. And also it could be unretainable collection.
+    // But I think you wanted me to do this
+    public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
+        return persons1.retainAll(persons2);
+    }
+
+    // count() is more convenient
+    public long countEven(Stream<Integer> numbers) {
+        return numbers.filter(num -> num % 2 == 0).count();
+    }
+
+    // Integer.class has it's underlaying value as a hash code itself. So hash set will be sorted on
+    // a creation
+    public static void listVsSet() {
+        List<Integer> integers =
+                IntStream.rangeClosed(1, 10000).boxed().collect(Collectors.toList());
+        List<Integer> snapshot = new ArrayList<>(integers);
+        Collections.shuffle(integers);
+        Set<Integer> set = new HashSet<>(integers);
+        assert snapshot.toString().equals(set.toString());
+    }
+
+    public static void main(String[] argv) {
+        listVsSet();
+    }
 }
